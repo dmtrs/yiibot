d797c90a92aec8421bc253a33ee62996ec216957
Dimitrios Meggidis <tydeas.dr@gmail.com>
2010-06-13 06:55:08 +0300

    yiimain.php

--- yiimain.php
+++ yiimain.php
@@ -2,24 +2,53 @@
 require_once('./yiidocdb.php');
 
 $db = new DOCDb();
-printClass($db->selectClass("CController"));
+$m = new DOCMain();
+$m->printClass($db->selectClass("CController"));
+$m->printClass($db->selectClass("CController") , "l" );
 
 
-/**
- * This method formats and print class details.
- * @paran array an array retuned from DOCDb::selectClass()
- **/
-function printClass( $class  ) {
-	foreach( $class as $name=>$value  ) {
-		$label = substr( $name , 3 );
-		if( $label=="inheritance" ) {
-			$ex = explode( "»" , $value );
-			$value = "";
-			foreach( $ex as $k => $v ) {
-				$value.= trim( $v )."->";
+class DOCMain {
+	public $params;
+	public function __construct() {
+		$this->params=$this->initParams();	
+	}
+	public function initParams() {
+		return array(
+			'n'=>'Name',
+			'p'=>'Package',
+			'i'=>'Inheritance',
+			'c'=>'Subclasses',
+			's'=>'Since',
+			'v'=>'Version',
+			'd'=>'Description',
+			'l'=>'Link'
+		);
+	}
+
+	/**
+	 * This method formats and print class details.
+	 * @paran array an array retuned from DOCDb::selectClass()
+	 **/
+	public function printClass( $class , $what="ndl" ) {
+		$fclass = array();
+		foreach( $class as $name=>$value  ) {
+			$name = substr( $name , 3 );
+			if( $name == "inheritance" ) {
+				$ex = explode( "»" , $value );
+				$value = "";
+				foreach( $ex as $k => $v ) {
+					$value.= trim( $v )."->";
+				}
+				$value = substr( $value , 0 , -2  );
 			}
-			$value = substr( $value , 0 , -2  );
+			$fclass[ucfirst( $name )] = $value;
+		}		
+		$print = str_split( $what );
+		foreach( $print as $k=>$value ) {
+			$att =  $this->params[$value];
+			echo "<".$att."> ".$fclass[$att]." ";
 		}
-		echo ucfirst( $label ).": ".$value."\n";
+		
+		
 	}
 }
